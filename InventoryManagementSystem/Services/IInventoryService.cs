43360363f8d235e4379b4c6b22147e407eb5cb8f
AnandharajRamalingam <btechanandh@gmail.com>
2026-09-11 21:10:56 +0530

    Format IInventoryService interface methods

--- InventoryManagementSystem/Services/IInventoryService.cs
+++ InventoryManagementSystem/Services/IInventoryService.cs
@@ -4,10 +4,10 @@ namespace InventoryManagementSystem.Services
 {
     public interface IInventoryService
     {
-        System.Threading.Tasks.Task<System.Collections.Generic.List<Product>> GetAllAsync();
-        System.Threading.Tasks.Task<Product?> GetByIdAsync(System.Guid id);
-        System.Threading.Tasks.Task AddAsync(Product product);
-        System.Threading.Tasks.Task UpdateAsync(Product product);
-        System.Threading.Tasks.Task DeleteAsync(System.Guid id);
+    Task<System.Collections.Generic.List<Product>> GetAllAsync();
+      Task<Product?> GetByIdAsync(System.Guid id);
+      Task AddAsync(Product product);
+      Task UpdateAsync(Product product);
+      Task DeleteAsync(System.Guid id);
     }
 }
